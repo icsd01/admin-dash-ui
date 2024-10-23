fe04563ffdd8c96dae53278d66fbb1b8692105e3
iychiangsd@gmail.com <iychiangsd@gmail.com>
2024-10-23 15:37:56 -0700

    add components, pages, and data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-import { Button } from "@/components/ui/button";
+import DashboardCard from "@/components/dashboard/DashboardCard";
+import PostsTable from "@/components/posts/PostsTable";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 
 export default function Home() {
   return (
     <>
-    <h1 className='text-2xl'>Dashboard</h1>
+      <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+        <DashboardCard
+          title="Posts"
+          icon={<Newspaper className="text-slate-500" size={48} />}
+          count={100}
+        />
+        <DashboardCard
+          title="Categories"
+          icon={<Folder className="text-slate-500" size={48} />}
+          count={12}
+        />
+        <DashboardCard
+          title="Users"
+          icon={<User className="text-slate-500" size={48} />}
+          count={750}
+        />
+        <DashboardCard
+          title="Comments"
+          icon={<MessageCircle className="text-slate-500" size={48} />}
+          count={1200}
+        />
+      </div>
+      <PostsTable title="Latest Posts" />
     </>
   );
 }
